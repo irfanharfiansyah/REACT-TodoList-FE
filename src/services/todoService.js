f69c353f76694e7d20e49b9d32c167babe236423
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2021-10-07 23:23:07 +0700

    Build Todo service

---
+++ src/services/todoService.js
@@ -0,0 +1,45 @@
+const baseUrl = `${process.env.REACT_APP_API_URL}/todos`;
+
+export const loadTodos = () => {
+    return fetch(baseUrl).then((res) => res.json());
+}
+
+export const getTodo = (id) => {
+    return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+}
+export const createTodo = (id) => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: getTodo.title,
+            completed: getTodo.completed
+        }),
+        
+    }).then((res) => res.json());
+
+}
+export const updateTodo = (id) => {
+    return fetch(`${baseUrl}/${todo.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: todo.id,
+            title: todo.title,
+            completed: todo.completed
+        }),
+        
+    }).then((res) => res.json());
+
+};
+
+export const deleteTodo = (id) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: "DELETE",
+    }).then(res => res.json());
+    
+}
