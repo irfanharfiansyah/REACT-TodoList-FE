a0aa9a8603809cb5b492d9d4b1f0a63cf9093156
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2021-10-08 12:56:47 +0700

    Create todo Tab

---
+++ src/components/TodoTab.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect} from "react";
+import { Tabs, Layout, Row, Col, List } from 'antd';
+import TodoItem from './TodoItem';
+
+const TodoTab = ({todos, onTodoRemoval, onTodoToggle }) => {
+
+    return(
+        <>
+        <List 
+            locale = {{ emptyText: "There's nothing to do :(", }}
+            dataSource = {todos}
+            renderItem = {(todos) => {
+                <TodoItem
+                    todo = {todo}
+                    onTodoToggle = {onTodoToggle}
+                    onTodoRemoval = {onTodoRemoval}
+                />
+            }}
+            pagination = {{ 
+                position:'bottom',
+                pageSize: 10,
+             }}/>
+        </>
+    )
+}
